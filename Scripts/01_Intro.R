e519744470b641c395986014d49a4fbc68c6ecf8
kevin007epn <kevin.apolo@epn.edu.ec>
2025-04-16 15:43:27 +0000

    cambios

--- Scripts/01_Intro.R
+++ Scripts/01_Intro.R
@@ -1,7 +1,26 @@
 # Primer script del curso
 
+# Configuraciones iniciales
+179/5378641
+options(scipen = 999)# elimina notacion cientifica
+options(digits = 4)# cambiar cantidad de digitos decimales significativos
+
+# Instalacion de paquetes
+install.packages("data.table")
+install.packages("tidyverse", dependencies = TRUE)
+library(data.table)
+library(tidyverse)
+
+#Rprofile
+if(!file.exists("~/.Rprofile")){
+  file.create("~/.Rprofile")
+} else {
+  file.edit("~/.Rprofile")
+}
+
 # Vectores
 x <- c(2,5,8,12)
 sum(x)
 mean(x)
 
+x %>% sum()
